2930449cd53c96ce129160167c4c961d59a5dd4a
sara <shksara2000@gmail.com>
2023-09-18 12:00:06 +0530

    Update

--- Controller/userController.js
+++ Controller/userController.js
@@ -8,13 +8,13 @@ const saltRound = 10
 const register = async(req, res) => {
     const details = req.body;
     //    console.log(details)
-    const find = arr.find((item) => details.email === item.email)
-    if (find) {
+    const comp = arr.find((item) => details.email === item.email)
+    if (comp) {
         return res.send({ Msg: "User already register" })
     }
     // const hashPassword=bcrypt.hash(originalPasswaord,saltRound);
-    const generatesalt = bcrypt.genSaltSync(10)
-    console.log("Random Sting Generated :", generatesalt)
+    // const generatesalt = bcrypt.genSaltSync(10)
+    // console.log("Random Sting Generated :", generatesalt)
 
     const hashPassword = await bcrypt.hash(details.password, saltRound);
     const data = {
@@ -24,20 +24,20 @@ const register = async(req, res) => {
         password: hashPassword,
     };
     arr.push(data)
-    const token=jwt.sign({email:details.email},process.env.secretkey,{expiresIn:"2d"})
+    const token=jwt.sign({email:details.email},process.env.secretkey,{expiresIn:"2 days"})
     res.send({ Msg: "User is register", result: data,token:token })
 }
 const login = async (req, res) => {
     const details = req.body;
-    const find = arr.find((item) => details.email === item.email)
-    if (!find) {
+    const Match = arr.find((item) => details.email === item.email)
+    if (!Match) {
         return res.send({ Msg: "User is Not Register....!" })
     }
-    const validate = await bcrypt.compare(details.password, find.password);
+    const validate = await bcrypt.compare(details.password, Match.password);
     if (!validate) {
         return res.status(401).send({ Msg: "User Password is Wrong" })
     }
-    const token=jwt.sign({email:details.email},process.env.secretkey,{expiresIn:"2d"})
+    const token=jwt.sign({email:details.email},process.env.secretkey,{expiresIn:"2 days"})
     return res.status(200).send({ Msg: "User is log in succesfully",token:token })
 }
 
--- index.js
+++ index.js
@@ -6,16 +6,11 @@ dotenv.config()
 // const validated = require("./Middleware/useMiddleware");
 
 const app=express();
-const middleware=(req,res,next)=>{
-    console.log("This is Appilcation Middleware")
-    next();
-}
 app.use(cors({
     origin:"*"
 }))
 const port=process.env.port
 app.use(express.json())
-app.use(middleware)
 app.use("/api/category",route)
 app.listen(port,()=>{
     console.log(`Server is running in port.${port}`)
